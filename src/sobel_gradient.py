b9c16c40fb941a1ac9ec28149cf16abec3b2beaa
TaliaFlor <talianadegia@gmail.com>
2022-06-17 14:09:24 -0300

    refactor original code for simplification #minor

--- src/sobel_gradient.py
+++ src/sobel_gradient.py
@@ -5,40 +5,54 @@
 import sys
 
 import cv2 as cv
+from numpy import ndarray
+
+# Global Variables
+WINDOW_NAME = 'Sobel Demo - Simple Edge Detector'
+SCALE = 1
+DELTA = 0
+KERNEL_SIZE = 3
+DDEPTH = cv.CV_16S
+DEFAULT_IMAGE = '../data/lena.png'
 
 
-def main(argv):
-    # First we declare the variables we are going to use
-    window_name = 'Sobel Demo - Simple Edge Detector'
-    scale = 1
-    delta = 0
-    ddepth = cv.CV_16S
+def get_grad(src_image: ndarray, x_order: int, y_order: int) -> ndarray:
+    return get_scharr_grad(src_image, x_order, y_order) if KERNEL_SIZE == 3 \
+        else get_sobel_grad(src_image, x_order, y_order)
 
+
+def get_sobel_grad(src_image: ndarray, x_order: int, y_order: int) -> ndarray:
+    return cv.Sobel(src=src_image, ddepth=DDEPTH, dx=x_order, dy=y_order, ksize=KERNEL_SIZE, scale=SCALE, delta=DELTA)
+
+
+def get_scharr_grad(src_image: ndarray, x_order: int, y_order: int) -> ndarray:
+    return cv.Scharr(src=src_image, ddepth=DDEPTH, dx=x_order, dy=y_order)
+
+
+def main(argv: list[str]) -> None:
     # Get image path
-    image_name = argv[0] if len(argv) > 0 else '../data/lena.png'
+    image_name = argv[0] if len(argv) > 0 else DEFAULT_IMAGE
 
     # Load the image
-    src = cv.imread(image_name, cv.IMREAD_COLOR)
+    src_image = cv.imread(image_name, cv.IMREAD_COLOR)
 
-    # Check if image is loaded fine
-    if src is None:
+    # Check if image is loaded
+    if src_image is None:
         print('Error opening image!')
         print(f'Usage: sobel_gradient.py [{image_name}] \n')
-        return -1
+        return
 
-    # Remove noise by blurring with a Gaussian filter ( kernel size = 3 )
-    src = cv.GaussianBlur(src, (3, 3), 0)
+    # Remove noise by blurring with a Gaussian filter
+    src_image = cv.GaussianBlur(src_image, (KERNEL_SIZE, KERNEL_SIZE), 0)
 
     # Convert the image to grayscale
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    gray_image = cv.cvtColor(src_image, cv.COLOR_BGR2GRAY)
 
     # Gradient-X
-    # grad_x = cv.Scharr(gray,ddepth,1,0)
-    grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+    grad_x = get_grad(src_image=gray_image, x_order=1, y_order=0)
 
     # Gradient-Y
-    # grad_y = cv.Scharr(gray,ddepth,0,1)
-    grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+    grad_y = get_grad(src_image=gray_image, x_order=0, y_order=1)
 
     # converting back to uint8
     abs_grad_x = cv.convertScaleAbs(grad_x)
@@ -47,10 +61,8 @@ def main(argv):
     # Total Gradient (approximate)
     grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
 
-    cv.imshow(window_name, grad)
-    cv.waitKey(0)
-
-    return 0
+    cv.imshow(WINDOW_NAME, grad)
+    cv.waitKey(0)  # esc
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-def test_main():
-    pass
-
-
-def test_main2():
-    pass
